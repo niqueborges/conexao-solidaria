4442045541cc10f677be53a26cdf7cafda82572b
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-10-25 17:58:52 -0300

    feat: add update institution handler

--- conexao_solidaria/api/v1/handlers/institution.py
+++ conexao_solidaria/api/v1/handlers/institution.py
@@ -2,7 +2,7 @@ import json
 from typing import Any
 from core.exceptions import ItemAlreadyExistsException
 from domain.services.institution import InstitutionService
-from infra.schemas.institutions import InstitutionIn
+from infra.schemas.institutions import InstitutionIn, UpdateInstitution
 from aws_lambda_powertools.utilities.parser import parse, ValidationError
 from aws_lambda_powertools.utilities.typing import LambdaContext
 from utils.build import build_http_response
@@ -45,5 +45,25 @@ def retrieve(event: dict, context: LambdaContext) -> dict[str, Any]:
     if not institution:
         return build_http_response(
             status_code=404, body={"detail": "Item does not extist."}
+        )
+    return build_http_response(status_code=200, body=institution)
+
+
+def update(event: dict, context: LambdaContext) -> dict[str, Any]:
+    """Updates the specified fields of a record in Dynamo."""
+    try:
+        # Retrieves 'cnpj' from the URL
+        cnpj = event["pathParameters"]["cnpj"]
+        # Validates the body by parsing to UpdateInstitution
+        data = parse(event=json.loads(event.get("body")), model=UpdateInstitution)
+    except ValidationError as exc:
+        return build_http_response(
+            status_code=400, body={"ValidationError": exc.errors(include_url=False)}
+        )
+    institution = InstitutionService.update(cnpj=cnpj, data=data)
+
+    if not institution:
+        return build_http_response(
+            status_code=404, body={"detail": "Item does not extist. Unable to update."}
         )
     return build_http_response(status_code=200, body=institution)
