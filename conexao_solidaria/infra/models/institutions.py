75c4cae2cddc6c43ec944560c661f4532c05f7ea
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-10-25 17:34:15 -0300

    feat: add InstitutionModel for the dynamo table

---
+++ conexao_solidaria/infra/models/institutions.py
@@ -0,0 +1,28 @@
+from pynamodb.models import Model
+from pynamodb.attributes import UnicodeAttribute, NumberAttribute, BooleanAttribute
+from core.config import settings
+
+
+class InstitutionModel(Model):
+    """PynamoDB model definition for the Institutions table"""
+
+    class Meta:
+        table_name = settings.TABLE_NAME
+        region = settings.REGION_NAME
+        billing_mode = settings.BILLING_MODE
+
+    cnpj = UnicodeAttribute(hash_key=True, null=False)
+    id = UnicodeAttribute(null=False)
+    token = UnicodeAttribute(null=False)
+    name = UnicodeAttribute(null=False)
+    email = UnicodeAttribute(null=False)
+    phone_number = UnicodeAttribute(null=False)
+    cep = UnicodeAttribute(null=False)
+    region = UnicodeAttribute(null=False)
+    state = UnicodeAttribute(null=False)
+    address_number = NumberAttribute(null=False)
+    confirmation_audio = UnicodeAttribute(null=False)
+    image = UnicodeAttribute(null=False)
+    about = UnicodeAttribute(null=False)
+    verified = BooleanAttribute(default=False, null=False)
+    site = UnicodeAttribute(null=True)
