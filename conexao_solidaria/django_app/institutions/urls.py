ace32e022b2f77673ab8d397218e74504a10b696
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-10-27 15:27:51 -0300

    feat: add view and url for detail instution page

--- conexao_solidaria/django_app/institutions/urls.py
+++ conexao_solidaria/django_app/institutions/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import HomeView, InstitutionListView
+from .views import HomeView, InstitutionListView, DetailInstitutionView
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("institutions/", InstitutionListView.as_view(), name="institutions"),
+    path(
+        "institutions/<str:cnpj>/", DetailInstitutionView.as_view(), name="institution"
+    ),
 ]
--- conexao_solidaria/django_app/institutions/views.py
+++ conexao_solidaria/django_app/institutions/views.py
@@ -24,3 +24,17 @@ class InstitutionListView(View):
 
         institutions = get_endpoint_data(endpoint)
         return render(request, "institutions.html", {"institutions": institutions})
+
+
+class DetailInstitutionView(View):
+    """
+    View responsible for displaying the detailed data of an institution
+    obtained through the specified endpoint.
+    """
+
+    def get(self, request, cnpj):
+        endpoint = settings.GET_INSTITUTION.format(cnpj=cnpj)
+
+        institution = get_endpoint_data(endpoint)
+
+        return render(request, "detail_institution.html", {"institution": institution})
