735aa0a12ede8c42f88a052bd139564141a3e042
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-10-27 15:17:45 -0300

    feat: add view and url for institutions page

--- conexao_solidaria/django_app/institutions/urls.py
+++ conexao_solidaria/django_app/institutions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, InstitutionListView
 
 urlpatterns = [
-    path("", HomeView.as_view(), name="home")
+    path("", HomeView.as_view(), name="home"),
+    path("institutions/", InstitutionListView.as_view(), name="institutions"),
 ]
--- conexao_solidaria/django_app/institutions/views.py
+++ conexao_solidaria/django_app/institutions/views.py
@@ -1,4 +1,8 @@
-from django.views.generic import TemplateView
+from app import settings
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
+
+from .utils import get_endpoint_data
 
 
 class HomeView(TemplateView):
@@ -7,3 +11,16 @@ class HomeView(TemplateView):
     """
 
     template_name = "base.html"
+
+
+class InstitutionListView(View):
+    """
+    View responsible for displaying all institutions registered at the
+    specified endpoint.
+    """
+
+    def get(self, request):
+        endpoint = settings.GET_INSTITUTIONS
+
+        institutions = get_endpoint_data(endpoint)
+        return render(request, "institutions.html", {"institutions": institutions})
