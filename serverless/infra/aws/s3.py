d3a9b6f79796cd9f575c27f230b7d913d1f0bc08
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-10-30 15:06:58 -0300

    refactor: remove S3 unused methods

--- serverless/infra/aws/s3.py
+++ serverless/infra/aws/s3.py
@@ -1,24 +1,12 @@
 import boto3
-from utils.date import format_date
 from core.config import settings
 
 
 class S3:
+    """Provides methods to interact with AWS S3 for storing and retrieving objects."""
+
     def __init__(self) -> None:
         self.client = boto3.client("s3", region_name=settings.REGION_NAME)
-
-    def get_object_url(self, bucket: str, key: str) -> str:
-        """Returns the object's URL."""
-        return f"https://{bucket}.s3.amazonaws.com/{key.replace(' ', '+')}"
-
-    def get_object_metadata(self, bucket: str, key: str) -> dict:
-        """Retrieves the object's metadata."""
-        return self.client.head_object(Bucket=bucket, Key=key)
-
-    def get_created_timestamp(self, bucket: str, key: str) -> str:
-        """Returns the object's last modified date."""
-        metadata = self.get_object_metadata(bucket, key)
-        return format_date(metadata["LastModified"])
 
     def put_object(self, bucket: str, body: bytes, key: str, contentType: str) -> None:
         """Stores the file in the bucket."""
