2eb9b87a722d5322cbc80db89d80bcffe0b78a16
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-11-15 11:21:42 -0300

    feat: add process_dialog_hook logic in TipsIntent

--- chatbot/backend/intents/tips.py
+++ chatbot/backend/intents/tips.py
@@ -2,7 +2,7 @@ import os
 
 from services.api import ApiClient
 from services.aws import AmazonServices
-from utils.slots import get_slot_value
+from utils.slots import get_slot_value, check_slot_filling
 from utils.responses import LexResponses
 
 
@@ -11,11 +11,21 @@ class TipsIntent:
     Class responsible for handling events related to the TipsIntent intent.
     """
 
+    validation_rules = {"TipType": r"^[1-4]$"}
+
+    tips = [
+        "Qual instituição escolher?",
+        "Como doar?",
+        "Como achar instituições confiáveis?",
+        "Qual tipo de instituição doar?",
+    ]
+
     def __init__(self, event: dict) -> None:
         self.event = event
 
     def process_dialog_hook(self) -> LexResponses:
-        pass
+        slots = self.event["sessionState"]["intent"]["slots"]
+        return check_slot_filling(slots, self.event, self.__class__.validation_rules)
 
     def process_full_fillment(self) -> LexResponses:
         slots = self.event["sessionState"]["intent"]["slots"]
@@ -25,7 +35,7 @@ class TipsIntent:
 
         prompt = slot_values["TipType"]
         print(prompt)
-        data = {"topic": slot_values["TipType"]}
+        data = {"topic": self.__class__.tips[int(prompt) - 1]}
 
         api_client = ApiClient(os.getenv("BASE_URL"))
         amazon_service = AmazonServices(api_client)
