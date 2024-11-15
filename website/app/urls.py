54018737427058fb95cc7acde7950a136e6a435b
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-11-15 17:49:32 -0300

    feat: add bot app url inclusion to website urls

--- website/app/urls.py
+++ website/app/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("institutions.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", include("institutions.urls")),
+    path("", include("bot.urls")),
+]
