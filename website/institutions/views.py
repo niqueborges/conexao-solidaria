e2d9896bb3a2add531a524e5ea532a1b4780f671
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-11-18 18:23:31 -0300

    feat: add get verified stitutions step

--- website/institutions/views.py
+++ website/institutions/views.py
@@ -21,9 +21,11 @@ class InstitutionListView(View):
 
     def get(self, request):
         endpoint = settings.GET_INSTITUTIONS
-        institutions = fetch_data(endpoint)
-
-        paginator = Paginator(institutions, 6)
+        all_institutions = fetch_data(endpoint).get("institutions", [])
+        verified_institutions = [
+            institution for institution in all_institutions if institution["verified"]
+        ]
+        paginator = Paginator(verified_institutions, 6)
         page_number = request.GET.get("page", 1)
         page_object = paginator.get_page(page_number)
 
@@ -49,3 +51,19 @@ class TermsOfUseView(TemplateView):
     """
 
     template_name = "terms_of_use.html"
+
+
+class FilterStateInstitutionView(View):
+    def get(self, request):
+        paginator = Paginator("change", 6)
+        page_number = request.GET.get("page", 1)
+        page_object = paginator.get_page(page_number)
+        return render(request, "institutions.html", {"page_object": page_object})
+
+
+class FilterRegionInstitutionView(View):
+    def get(self, request):
+        paginator = Paginator("change", 6)
+        page_number = request.GET.get("page", 1)
+        page_object = paginator.get_page(page_number)
+        return render(request, "institutions.html", {"page_object": page_object})
