1ef61564adee3a51146a9320c007b88851129a1c
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2024-11-18 18:25:20 -0300

    feat: add filter institutions url pattern

--- website/institutions/urls.py
+++ website/institutions/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import DetailInstitutionView, HomeView, InstitutionListView, TermsOfUseView
+
+from .views import (
+    DetailInstitutionView,
+    HomeView,
+    InstitutionListView,
+    TermsOfUseView,
+    FilterRegionInstitutionView,
+    FilterStateInstitutionView,
+)
 
 
 urlpatterns = [
@@ -7,6 +15,16 @@ urlpatterns = [
     path("institutions/", InstitutionListView.as_view(), name="institutions"),
     path(
         "institutions/<str:cnpj>/", DetailInstitutionView.as_view(), name="institution"
+    ),
+    path(
+        "institutions/filter/<str:state>",
+        FilterStateInstitutionView.as_view(),
+        name="institution",
+    ),
+    path(
+        "institutions/filter/<str:region>",
+        FilterRegionInstitutionView.as_view(),
+        name="institution",
     ),
     path("terms-of-use/", TermsOfUseView.as_view(), name="terms_of_use"),
 ]
