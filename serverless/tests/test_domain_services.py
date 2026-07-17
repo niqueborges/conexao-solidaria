8b972b70b435a1dbd0aadfcc386a566a8401bdb9
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2026-07-17 14:43:59 -0300

    test: add unit tests for institution service operations

--- serverless/tests/test_domain_services.py
+++ serverless/tests/test_domain_services.py
@@ -1,6 +1,6 @@
 import pytest
 from domain.services.institution import InstitutionService
-from infra.schemas.institutions import CreateInstitution
+from infra.schemas.institutions import CreateInstitution, UpdateInstitution
 from core.exceptions import InstitutionAlreadyExistsException
 from infra.repositories.institution import PynamoDBInstitutionRepository
 
@@ -84,3 +84,110 @@ def test_get_institution_success():
 
     response = service.get("12345678901234")
     assert response["name"] == "Test Institution"
+
+def test_get_all_institutions():
+    repository = PynamoDBInstitutionRepository()
+    service = InstitutionService(repository=repository)
+    
+    data = CreateInstitution(
+        cnpj="99999999999999",
+        name="All Institution",
+        email="all@test.com",
+        phone_number="11999999999",
+        cep="00000000",
+        region="Sul",
+        state="RS",
+        address="Rua",
+        address_number=1,
+        city="PoA",
+        neighborhood="Centro",
+        confirmation_audio="http://example.com/audio.mp3",
+        image="http://example.com/image.png",
+        about="About",
+        site="http://www.test.com",
+    )
+    service.create(data)
+    
+    response = service.get_all()
+    assert "institutions" in response
+    assert len(response["institutions"]) > 0
+
+def test_query_institutions():
+    repository = PynamoDBInstitutionRepository()
+    service = InstitutionService(repository=repository)
+    
+    data = CreateInstitution(
+        cnpj="88888888888888",
+        name="Query Institution",
+        email="query@test.com",
+        phone_number="11999999999",
+        cep="00000000",
+        region="Norte",
+        state="AM",
+        address="Rua",
+        address_number=1,
+        city="Manaus",
+        neighborhood="Centro",
+        confirmation_audio="http://example.com/audio.mp3",
+        image="http://example.com/image.png",
+        about="About",
+        site="http://www.test.com",
+    )
+    service.create(data)
+    
+    response = service.query(region="Norte", state="AM")
+    assert "institutions" in response
+    assert len(response["institutions"]) > 0
+
+def test_update_institution():
+    repository = PynamoDBInstitutionRepository()
+    service = InstitutionService(repository=repository)
+    
+    data = CreateInstitution(
+        cnpj="77777777777777",
+        name="Old Name",
+        email="old@test.com",
+        phone_number="11999999999",
+        cep="00000000",
+        region="Norte",
+        state="AM",
+        address="Rua",
+        address_number=1,
+        city="Manaus",
+        neighborhood="Centro",
+        confirmation_audio="http://example.com/audio.mp3",
+        image="http://example.com/image.png",
+        about="About",
+        site="http://www.test.com",
+    )
+    service.create(data)
+    
+    update_data = UpdateInstitution(name="New Name")
+    response = service.update("77777777777777", update_data)
+    assert response["name"] == "New Name"
+
+def test_delete_institution():
+    repository = PynamoDBInstitutionRepository()
+    service = InstitutionService(repository=repository)
+    
+    data = CreateInstitution(
+        cnpj="66666666666666",
+        name="Delete Me",
+        email="del@test.com",
+        phone_number="11999999999",
+        cep="00000000",
+        region="Norte",
+        state="AM",
+        address="Rua",
+        address_number=1,
+        city="Manaus",
+        neighborhood="Centro",
+        confirmation_audio="http://example.com/audio.mp3",
+        image="http://example.com/image.png",
+        about="About",
+        site="http://www.test.com",
+    )
+    service.create(data)
+    
+    result = service.delete("66666666666666")
+    assert result is True
